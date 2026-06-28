0e9045a7a9697f799ed0e2bc68bbb2b7f00ad12f
kaushikharsh99 <harshkaushik11000@gmail.com>
2026-06-28 22:54:21 +0530

    Fixed working of qwen3 30B moe after modular architecture

--- models/qwen3_moe.py
+++ models/qwen3_moe.py
@@ -1,27 +1,31 @@
 from models.base import BaseModelAdapter
 from execution.layouts import Qwen3LayerLayout
 from transformers.models.qwen3_moe.modeling_qwen3_moe import create_causal_mask
-
+from cache.kv_cache import KVCache
 class Qwen3MoeAdapter(BaseModelAdapter):
 
     def __init__(self, model, loader):
         self.model = model
         self.loader = loader
 
+    @property
+    def text_model(self):
+        return self.model.model
+
     def create_layer_layout(self):
         return Qwen3LayerLayout()
     
     def load_config(self):
-        return self.model.config
+        return self.text_model.config
 
     def create_meta_model(self):
         return self.model
 
     def embed(self, input_ids):
-        return self.model.model.embed_tokens(input_ids)
+        return self.text_model.embed_tokens(input_ids)
 
     def layers(self):
-        return self.model.model.layers
+        return self.text_model.layers
 
     def forward_layer(
         self,
@@ -31,7 +35,7 @@ class Qwen3MoeAdapter(BaseModelAdapter):
         position_ids,
         attention_mask,
     ):
-        layer = self.model.model.layers[layer_id]
+        layer = self.text_model.layers[layer_id]
         outputs = layer(
             hidden_states=hidden,
             past_key_values=kv_cache,
@@ -41,14 +45,14 @@ class Qwen3MoeAdapter(BaseModelAdapter):
         return outputs[0], kv_cache.get(layer_id) if kv_cache is not None else None
 
     def final_norm(self, hidden):
-        return self.model.model.norm(hidden)
+        return self.text_model.norm(hidden)
 
     def lm_head(self, hidden):
         return self.model.lm_head(hidden)
 
     def create_attention_mask(self, hidden, kv_cache, position_ids):
         return create_causal_mask(
-            config=self.model.config,
+            config=self.text_model.config,
             inputs_embeds=hidden,
             attention_mask=None,
             past_key_values=kv_cache,
@@ -56,16 +60,25 @@ class Qwen3MoeAdapter(BaseModelAdapter):
         )
 
     def rotary_embeddings(self, hidden, position_ids):
-        return self.model.model.rotary_emb(hidden, position_ids=position_ids)
+        return self.text_model.rotary_emb(
+            hidden,
+            position_ids=position_ids,
+        )
 
     @property
     def num_layers(self):
-        return self.model.config.num_hidden_layers
+        return self.text_model.config.num_hidden_layers
 
     @property
     def hidden_size(self):
-        return self.model.config.hidden_size
+        return self.text_model.config.hidden_size
 
     @property
     def is_moe(self):
         return True
+
+    def create_cache(
+        self,
+        max_seq_len,
+    ):
+        return KVCache(max_seq_len)
